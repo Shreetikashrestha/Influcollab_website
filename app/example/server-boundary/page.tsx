2c79beb85b54b13d438dffbfd205d82b38ed4620
ShreetikaShrestha <shreetika59@gmail.com>
2026-02-21 23:07:57 +0545

    example added

--- app/example/server-boundary/page.tsx
+++ app/example/server-boundary/page.tsx
@@ -1,14 +1,16 @@
 import { handleExample } from "@/lib/actions/example-action";
 //page can be async
-export default async function Page(){
+export default async function Page() {
     const result = await handleExample();
+    /*
     if(result.success){
         throw new Error("Error");
     }
+    */
 
     return (
         <div>
-            Example Server Boundary 
+            Example Server Boundary
         </div>
     )
 }
